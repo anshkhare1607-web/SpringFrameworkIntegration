2a8a7a324297fd91483e13a1e8a6d0d65f73c3af
anshkhare1607-web <anshkhare1607@gmail.com>
2026-03-30 21:54:41 +0530

    Testing file for repository

---
+++ Quantity-Measurement-App/src/test/java/com/app/quantitymeasurement/repository/QuantityMeasurementRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.app.quantitymeasurement.repository;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityRepositoryTest {
+
+    @Test
+    void testBasic() {
+
+        // Just dummy test (beginner level)
+        assertTrue(true);
+    }
+}
