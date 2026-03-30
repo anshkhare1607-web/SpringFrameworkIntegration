5ecb859c38f646d72a98cc55ab862a4eb0c10613
anshkhare1607-web <anshkhare1607@gmail.com>
2026-03-30 15:33:54 +0530

    Add global exception handler

---
+++ Quantity-Measurement-App/src/main/java/com/app/quantitymeasurement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.app.quantitymeasurement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationException() {
+        return "Invalid Input data Please check required fields.";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+}
