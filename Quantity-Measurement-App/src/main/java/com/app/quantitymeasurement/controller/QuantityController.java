ee2018ae11125818d4083ac3fb2cf6e778667718
anshkhare1607-web <anshkhare1607@gmail.com>
2026-05-13 21:56:00 +0530

    Update QuantityController.java

--- Quantity-Measurement-App/src/main/java/com/app/quantitymeasurement/controller/QuantityController.java
+++ Quantity-Measurement-App/src/main/java/com/app/quantitymeasurement/controller/QuantityController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/quantity")
+@RequestMapping("/quantity")
 public class QuantityController {
 
     private final QuantityService service;
